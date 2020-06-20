63a6af99eb44c158cceb92e3c3ebed977cc0866b
AntonBiryukovUofC <anton.biryukov@ucalgary.ca>
2020-06-20 14:50:17 -0600

    feat: added color quant using crayola 12 box

--- streamlit_stipple.py
+++ streamlit_stipple.py
@@ -8,15 +8,50 @@ import streamlit as st
 from scipy import ndimage as ndi
 from scipy.ndimage import zoom
 from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors, KNeighborsClassifier
 from sklearn.utils import shuffle
-
+from skimage import color
+from matplotlib.colors import to_rgb
 from src.rougier.stippler_utils import stipple, normalize
+from skimage.color import deltaE_ciede2000, deltaE_cie76
+
+NDENSITY = 200
+# black, brown, blue, green, orange, sky blue, red, red orange, violet, white, yellow, yellow green
+PALETTE = ('#49484C', '#6F5756', '#3D57D5', '#508D5D', '#E16B40',
+           '#64B1F2', '#CE465D', '#D95555', '#605CA6','#FFFFFF','#FDF453', '#87BE55')
+
+@st.cache
+def to_discrete_colors(img, palette=PALETTE):
+    # Convert to LAB
+    img_lab = color.rgb2lab(img)
+    h, w = img.shape[0], img.shape[1]
+    nn = KNeighborsClassifier(n_neighbors=1,
+                              n_jobs=8)
+    color_palette_train = np.expand_dims(np.array([to_rgb(c) for c in palette]).reshape(-1, 3), 1)
+
+    color_palette_train = color.rgb2lab(color_palette_train)
+    y = np.arange(color_palette_train.shape[0])
+    # Fit predict in lab space
+    nn.fit(color_palette_train[:, 0, :], y)
+    img_predicted = nn.predict(img_lab.reshape(-1, 3))
+    img_predicted = color_palette_train[img_predicted.reshape(h, w)][:, :, 0, :]
+    img_predicted = color.lab2rgb(img_predicted)
+    return img_predicted
+
+
+def preprocess_size(im, zoom_lvl):
+    im = zoom(im, zoom_lvl, order=0)
+    im = ndi.gaussian_filter(im, 1)
+    new_im = 1 - normalize(im)
+
+    return new_im
 
 
 def quantify_color(img_orig, n_colors):
     # Load Image and transform to a 2D numpy array.
     img = img_orig[:, :, [0, 1, 2]] / 255
-    w, h, d = original_shape = tuple(img.shape)
+
+    w, h, d = tuple(img.shape)
     assert d == 3
     image_array = np.reshape(img, (w * h, d))
 
@@ -58,7 +93,7 @@ def pick_color(points, img_compressed, hex=True):
     Y = np.maximum(np.minimum(Pi[:, 1], img_compressed.shape[0] - 1), 0)
     cols = img_compressed[Y, X, :]
     if hex:
-        hex_cols = np.array(cols.shape[0]*["#FFFFFF"])
+        hex_cols = np.array(cols.shape[0] * ["#FFFFFF"])
         for i in range(cols.shape[0]):
             hex_cols[i] = rgb2hex(cols[i, :])
         res = hex_cols
@@ -83,73 +118,57 @@ def create_stipple(density, n_point, n_iter, filename, threshold):
                                         force=True, dat_filename=dat_filename,
                                         png_filename=png_filename,
                                         pdf_filename=pdf_filename,
-                                        pointsize=ps)
+                                        pointsize=ps, n_density=NDENSITY)
     return density_new, points, bbox
 
 
 st.title('Stippling test')
-filename = st.sidebar.text_input(label='File location', value='./data/plants.jpg')
-n_point = st.sidebar.number_input(label=' N points', value=20000, max_value=50000, step=1000)
-n_colors = st.sidebar.number_input(label=' N colors', value=3, max_value=15, min_value = 2)
-
+filename = st.sidebar.text_input(label='File location', value='./data/rainbow-emoji.jpg')
+n_point = st.sidebar.number_input(label=' N points', value=5000, max_value=50000, step=1000)
+n_colors = st.sidebar.number_input(label=' N colors', value=3, max_value=15, min_value=2)
+quantify_color_flag = st.sidebar.checkbox(label='Quantify colors?', value=False)
 n_iter = st.sidebar.number_input(label=' N iters', value=3, max_value=50, step=2)
 threshold = st.sidebar.number_input(label=' Threshold', value=255., max_value=255., step=0.1, min_value=0.0)
 ps_min = st.sidebar.number_input(label='Min point size ', value=1., min_value=0.01, max_value=100.0)
 ps_max = st.sidebar.number_input(label='Max point size ', value=10., min_value=0.01, max_value=100.0)
 ps = (ps_min, ps_max)
 
-n_density = 200
-
+# Read the images in B/W and Full Color
 density = imageio.imread(filename, pilmode='L')
 img_original = imageio.imread(filename)
 
+img_discrete_palette = to_discrete_colors(img_original, palette=PALETTE)
+
 density_orig = density.copy()
-# We want (approximately) 500 pixels per voronoi region
-zoom_lvl = (n_point * n_density) / (density.shape[0] * density.shape[1])
+# We want (approximately) NDENSITY pixels per voronoi region
+zoom_lvl = (n_point * NDENSITY) / (density.shape[0] * density.shape[1])
 zoom_lvl = int(round(np.sqrt(zoom_lvl)))
 density = zoom(density, zoom_lvl, order=0)
 density = np.minimum(density, threshold)
 
 density_new, points, bbox = create_stipple(density, n_point, n_iter, filename, threshold)
 
-# Do a visual
-# st.write(points)
-# Plot density:
-st.write(density_new.shape)
-
 density_thresh = np.minimum(density, threshold)
 density_thresh = normalize(density_thresh)
-# density_thresh = density_thresh[::-1, :]
-
+# Convert to DF for altair viz
 points_df = pd.DataFrame(points, columns=['x', 'y'])
-
-
-def preprocess_size(im):
-    im = zoom(im, zoom_lvl, order=0)
-    im = ndi.gaussian_filter(im, 1)
-    new_im = 1 - normalize(im)
-
-    return new_im
-
-
-density_adjusted = preprocess_size(density_orig)
-
+density_adjusted = preprocess_size(density_orig, zoom_lvl=zoom_lvl)
+points_df['size'] = pick_size(points, density_adjusted, ps)
+st.markdown(f"###  Thresholded at {threshold} B/W source image")
 st.image(density_thresh, use_column_width=True)
-# st.image(density_new, use_column_width=True)
 
 # Quantify colors
-labels, kmeans, shape = quantify_color(img_original,
-                                       n_colors=n_colors)
-img_compressed = recreate_image(kmeans.cluster_centers_, labels, shape[0], shape[1])
-# Pick the size of the points using density as a guide:
-points_df['size'] = pick_size(points, density_adjusted, ps)
-# Pick the colors of the points using quantized space:
-# points_df['color'] = pick_color(points, img_compressed, hex=True)
-cols = pick_color(points, img_compressed, hex=True)
-if n_colors > 2:
+if (len(img_original.shape) > 2) & quantify_color_flag:
+    img_discrete_palette = to_discrete_colors(img_original, palette=PALETTE)
+    img_compressed = img_discrete_palette
+    st.markdown(f'### Image discretized into color palette \n{PALETTE}')
+    st.image(img_compressed, use_column_width=True)
+    cols = pick_color(points, img_compressed, hex=True)
     points_df['color'] = cols
 else:
     points_df['color'] = 'black'
+
+# Pick the size of the points using density as a guide:
 
 
 width = 800
@@ -158,11 +177,10 @@ ch = alt.Chart(data=points_df,
                width=width,
                height=height).encode(x=alt.X('x', axis=None), y=alt.Y('y', axis=None, sort=alt.SortOrder('descending')),
                                      size=alt.Size('size', legend=None, scale=alt.Scale(range=(ps[0], ps[1]))),
-                                     color=alt.Color('color',scale=None)).mark_point(filled=True, opacity=1.0)
-
+                                     color=alt.Color('color', scale=None)).mark_point(filled=True, opacity=1.0)
 ch = ch.configure_axis(grid=False).configure_view(strokeWidth=0)
+st.markdown('### Stippled image')
 st.altair_chart(ch)
-st.image(img_compressed,use_column_width=True)
 st.write(points_df['color'].value_counts())
 #     # Apply threshold onto image
 #     # Any color > threshold will be white
